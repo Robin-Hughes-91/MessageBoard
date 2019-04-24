b9abd9544df9f96724643b23ad712156fe3df93b
Robin Hughes <robin22391@hotmail.com>
2019-04-24 01:55:23 +0100

    adds heroku url for home.vue

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -70,7 +70,7 @@ import { EventBus } from '../event-bus.js';
 
 
 //this is the address for the API containing all the messages
-const  API_URL = 'http://localhost:1234/messages';
+const  API_URL = 'https://full-stack-gif-message-board.herokuapp.com/';
 
 
 //mounted method runs when homepage gets run
